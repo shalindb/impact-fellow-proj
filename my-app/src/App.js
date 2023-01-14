e72f7d3143d4873fd4bc9a089fbe7193c33107c2
Shalin Brahmbhatt <63821972+shalindb@users.noreply.github.com>
2023-01-14 17:46:04 -0500

    stupid ass search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {Button, Alert} from 'react-bootstrap';
 import {Container, Row} from 'react-bootstrap';
 import Grid from '@mui/material/Button';
+import { MDBInputGroup, MDBInput, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
 import * as Icon from 'react-bootstrap-icons';
 
 import NavbarComp from './Components/NavbarComp';
 
 function App() {
+  const [showSearchAlert, setShowSearchAlert] = useState(false);
   return (
     <div className="App">
-      
       <NavbarComp/>
       <div>
+
       <div className = "d-grid gap-3" >
       <Grid container spacing={2}>
       <Grid item xs={8}>
@@ -31,6 +33,16 @@ function App() {
       </Grid>
       </div>
       </div>
+      <Container>
+        <>
+          <MDBInputGroup>
+            <MDBInput label='Search' />
+            <MDBBtn onClick={() => setShowSearchAlert(true)} rippleColor='dark'>
+              <MDBIcon icon='search' />
+            </MDBBtn>
+          </MDBInputGroup>
+        </>
+      </Container>
     </div>
   );
 }
